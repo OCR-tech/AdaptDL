5558fd09c5b2319790338660dc571cdc829897f4
OCR-tech <s6010767@hotmail.com>
2025-05-27 19:23:25 +0700

    update files

--- app/main.py
+++ app/main.py
@@ -1,33 +1,7 @@
 # Import necessary modules
 import cv2
 import os
-# import sys
-# import argparse
 from src.config.configs import *
-
-
-# //===========================================//
-# def parse_arguments():
-
-#     parser = argparse.ArgumentParser(description="Example script using argparse.")
-#     parser.add_argument('--config', type=str, help='Path to config file')
-#     parser.add_argument('--device', type=str, default='cpu', help='Device to use (cpu/gpu)')
-#     parser.add_argument('--input', type=str, help='Input source (image/video)')
-#     parser.add_argument('--threshold', type=float, default=0.5, help='Detection threshold')
-#     args = parser.parse_args()
-
-#     print("Config file:", args.config)
-#     print("Device:", args.device)
-#     print("Input source:", args.input)
-#     print("Threshold:", args.threshold)
-
-#     # Example usage in your code
-#     if args.input:
-#         print(f"Processing input: {args.input} on device: {args.device}")
-#     else:
-#         print("No input source provided.")
-#     return args
-
 
 
 
@@ -45,15 +19,6 @@ def main():
 
     # This function is the entry point of the application.
     print("//*** main ***//")
-
-
-    # # Parse command line arguments
-    # args = parse_arguments()
-    # config = args.config
-    # device = args.device
-    # input_source = args.input
-    # threshold = args.threshold
-    # print(f"Parsed arguments: config={config}, device={device}, input_source={input_source}, threshold={threshold}")
 
 
 
@@ -116,4 +81,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()  # Call the main function to start the application
+    main()
