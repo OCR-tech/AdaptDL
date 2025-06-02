dd6b1cda6ac4c54a38a69d12ba2d11dfc90493ca
OCR-tech <s6010767@hotmail.com>
2025-06-02 08:15:28 +0700

    update files

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ import os
 from src.config.configs import *
 
 
-
-
 # //===========================================//
 def main():
     '''
@@ -20,63 +18,46 @@ def main():
     # This function is the entry point of the application.
     print("//*** main ***//")
 
+    # Check if the paths are valid
+    if not ((os.path.exists(imagepath)) or "-") or not (os.path.exists(videopath)) or not (os.path.exists(configpath))  or not (os.path.exists(modelpath)) or not (os.path.exists(classespath)):
+        print("Invalid system paths.")
+        os._exit(1)
+    else:
+        print("Valid system paths")
 
+        # # //================================//
+        # The main loop to continuously listen for voice commands and process them
+        while True:
+            # Initialize the system
+            # speak("System initialized. Please provide a command.")
+            # print("System initialized. Please provide a command.")
 
-    # # //================================//
-    # command = ""
-    # while command != "exit":
-    #     command = listen_for_commands()
-    #     print('command := ', command)
-    #     command = "start"
+            # Listen for voice commands
+            # command = listen_for_commands()
+            command = "start"
+            # command = "exit"
+            # command = "help"
 
-    #     if command == "start":
-    #         print('//=== ok ===//')
-    #         speak("ok")
-    #         detect = detector(imagepath, videopath, configpath, modelpath, classespath)
-    #         detect.onVideo()
-    #         # detect.onImage()
+            print(f"Voice Command: {command}")
+            # speak(command)
 
+            # Process the command
+            if command:
+                if command == "exit":
+                    print("Resources released. Exiting the program.")
+                    speak("exiting the system.")
+                    cv2.destroyAllWindows()
+                    os._exit(1)
+                elif command == "start":
+                    print("Start the system.")
+                    # speak("Start the system.")
+                    detect = detector(imagepath, videopath, configpath, modelpath, classespath)
+                    detect.onVideo()
+                    # detect.onImage()
 
-
-    # # //================================//
-    # The main loop to continuously listen for voice commands and process them
-    while True:
-        # Initialize the system
-        # speak("System initialized. Please provide a command.")
-        # print("System initialized. Please provide a command.")
-        # speak("listening for command ...")
-        # print("listening for command ...")
-
-        # Listen for voice commands
-        # command = listen_for_commands()
-        # command = "exit"
-        command = "start"
-        # command = "help"
-
-        print(f"Voice Command: {command}")
-        # speak(command)
-
-
-        # Process the command
-        if command:
-            if command == "exit":
-                print("Resources released. Exiting the program.")
-                speak("exiting the system.")
-                cv2.destroyAllWindows()
-                os._exit(1)
-            elif command == "start":
-                print("Start the system.")
-                # speak("Start the system.")
-                detect = detector(imagepath, videopath, configpath, modelpath, classespath)
-
-                # Uncomment the following line to process for image or video input
-                detect.onVideo()
-                # detect.onImage()
-
-            elif command == "help":
-                print("Available commands: start, help, exit")
-                speak("Available commands: start, help, exit")
-
+                elif command == "help":
+                    print("Available commands: start, help, exit")
+                    speak("Available commands: start, help, exit")
 
 
 
