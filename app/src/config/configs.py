9510c8f7b01f6568d96e37fee6bfebefbd5e0df6
OCR-tech <s6010767@hotmail.com>
2025-06-02 19:48:33 +0700

    update files

--- app/src/config/configs.py
+++ app/src/config/configs.py
@@ -6,12 +6,11 @@ import os
 # Specify the image file to be used.
 image = "pic1.jpg"
 # image = "pic2.jpg"
-# image = "pic3.jpg"
 
 # Specify the video file to be used.
-video = "video1.mp4"      # 3840x2160    30fps
-# video = "video2.mp4"    # 1920x1080    30fps
-# video = "video3.mp4"    # 1920x1080    30fps
+# video = "video1.mp4"      # 3840x2160    30fps
+video = "video2.mp4"    # 1920x1080    30fps
+
 
 # Define the base path for resources (images and videos).
 # path = 'D:/dataset/'
